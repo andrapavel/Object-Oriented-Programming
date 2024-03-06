fd7f29b36646efa2a76ea3d766f273eefd7ecda1
Andra Pavel <123322135+andrapavel@users.noreply.github.com>
2024-03-06 21:21:05 +0200

    Add files via upload

---
+++ Laboratories/Lab-6-7/Problem-1/Circle.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Shape.h"
+
+class Circle: public Shape
+{
+public:
+	Circle(float x, float y, float r);
+
+	float area();
+	std::string toString();
+
+private:
+	float m_radius;
+};
+
---
+++ Laboratories/Lab-6-7/Problem-1/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <crtdbg.h>
+
+#include "Shape.h"
+#include "Circle.h"
+#include "Square.h"
+
+void exercise1() 
+{
+	// TODO your code here
+	Circle c1{ 10, 10, 1 };
+	Shape* c2{ new Circle{0, 0, 100 } };
+	Circle c3{ -10, 0, 2 };
+
+	Shape *s1{ new Square{ 0, 0, 2 }};
+	Square s2{ 10, 20, 17 };
+	Square s3{ 9, 100, 0.3 };
+
+	// 1. create a polymorphic static array shapes with 6 elements that holds c1, c2, c3 and s1, s2 and s3
+
+	Shape* shapes[] = { &c1, c2, &c3, s1, &s2, &s3 };
+	const int numShapes = sizeof(shapes) / sizeof(shapes[0]);
+	for (int i = 0; i < numShapes; i++)
+		std::cout << shapes[i]->toString() << std::endl;
+
+	
+	// 2. display all the elements that have an area greater than 10
+
+	for (int i = 0; i < numShapes; i++)
+	{
+		if (shapes[i]->area() > 10)
+		{
+			std::cout << "Shape at index " << i << " has area greater than 10: ";
+			std::cout << shapes[i]->toString() << std::endl;
+		}
+	}
+
+	
+		
+	// 3. display all the circles from the array shapes
+
+	for (int i = 0; i < numShapes; i++)
+	{
+		if (dynamic_cast<Circle*>(shapes[i]))
+		{
+			std::cout << "Circle at index " << i << ": ";
+			std::cout << shapes[i]->toString() << std::endl;
+		}
+	}
+
+		
+	delete s1;
+	delete c2;
+}
+
+int main() 
+{
+
+	exercise1();
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
+
